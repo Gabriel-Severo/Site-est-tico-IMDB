801d0701441e76750c6a52d176a60ba5fb0774c4
gabriel-severo <gabriel.ivi@hotmail.com>
2020-05-02 23:06:23 -0300

    Now return array with the filter

--- nodejs-12/src/index.js
+++ nodejs-12/src/index.js
@@ -1,7 +1,15 @@
 const promotions = ['SINGLE LOOK', 'DOUBLE LOOK', 'TRIPLE LOOK', 'FULL LOOK'];
 
 function getShoppingCart(ids, productsList) {
-	return {};
+	const products = productsList.filter(product => {
+		for(id of ids){
+			if (product.id == id){
+				return true
+			}
+		}
+		return false
+	});
+	return products
 }
 
 module.exports = { getShoppingCart };
