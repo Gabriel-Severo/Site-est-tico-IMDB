0bac146a110c80dfeda543f97aef4b40d09c7324
gabriel-severo <gabriel.ivi@hotmail.com>
2020-05-03 00:05:51 -0300

    Add function getPromotion

--- nodejs-12/src/index.js
+++ nodejs-12/src/index.js
@@ -1,5 +1,15 @@
 const promotions = ['SINGLE LOOK', 'DOUBLE LOOK', 'TRIPLE LOOK', 'FULL LOOK'];
 
+function getPromotion(products){
+	const pro = []
+	for(let product of products){
+		if(!(pro.includes(product.category))){
+			pro.push(product.category)
+		}
+	}
+	return promotions[pro.length-1]
+}
+
 function getShoppingCart(ids, productsList) {
 	const products = productsList.filter(product => {
 		for(id of ids){
@@ -9,7 +19,10 @@ function getShoppingCart(ids, productsList) {
 		}
 		return false
 	});
-	return products
+	const productsMapped = products.map(product => {
+		return {category: product.category, name: product.name}
+	})
+	return {products: productsMapped, promotion: getPromotion(products)}
 }
 
 module.exports = { getShoppingCart };
